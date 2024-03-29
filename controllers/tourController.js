4fba7105c09c916db785b4f5a9626a2e1fd90b78
Pratik Tandel <pratik.tandel@1rivet.comgit>
2024-03-29 17:21:56 +0530

    feat: add tour, get tours, update tour, delete tour api added

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -1,81 +1,160 @@
-const fs = require('fs');
+const Tour = require('./../models/tourModel');
 
-const tours = JSON.parse(
-    fs.readFileSync(`${__dirname}/../dev-data/data/tours-simple.json`)
-);
+exports.checkID = (req, res, next, val) => {
+  const id = req.params.id * 1;
+  // if (id > tours.length) {
+  //   return res.status(404).json({
+  //     status: 'fail',
+  //     message: 'Invalid ID'
+  //   });
+  // }
+  next();
+};
 
-exports.getAllTours = (req, res, next) => {
+
+/**
+ * Middleware function to check if the request body contains the required fields.
+ *
+ * @param {Object} req - The request object
+ * @param {Object} res - The response object
+ * @param {Function} next - The next middleware function
+ * @return {Object} JSON response indicating the missing fields, or calls the next middleware
+ */
+exports.checkBody = (req, res, next) => {
+  if (!req.body.name || !req.body.price) {
+    return res.status(400).json({
+      status: 'fail',
+      message: 'Missing name or price'
+    });
+  }
+  next();
+};
+
+/**
+ * Retrieves all tours from the database and sends them as a JSON response.
+ *
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @param {Function} next - The next middleware function.
+ * @return {Promise<void>} The function does not return anything.
+ */
+exports.getAllTours = async (req, res, next) => {
+  try {
+    const tours = await Tour.find();
     res.status(200).json({
-        status: 'success',
-        results: tours.length,
-        data: {
-            tours
-        }
+      status: 'success',
+      results: tours.length,
+      data: {
+        tours
+      }
     });
-}
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
+};
 
-exports.getTour = (req, res) => {
-    const id = req.params.id * 1;
-    const tour = tours.find(el => el.id === id);
-
-    if (!tour) {
-        res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-
-        return;
-    }
+/**
+ * Retrieves a tour by its ID and sends it as a JSON response.
+ *
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @return {Promise<void>} The JSON response containing the tour data or an error message.
+ */
+exports.getTour = async (req, res) => {
+  try {
+    const tour = await Tour.findById(req.params.id);
+    // Tour.findOne({ _id: req.params.id });
 
     res.status(200).json({
-        status: 'success',
-        data: {
-            tour
-        }
+      status: 'success',
+      data: {
+        tour
+      }
     });
-}
-
-exports.createTour = (req, res) => {
-    const newId = tours[tours.length - 1].id + 1;
-    const newTour = Object.assign({ id: newId }, req.body);
-
-    tours.push(newTour);
-    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), err => {
-        res.status(201).json({
-            status: 'success',
-            data: {
-                tour: newTour
-            }
-        });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
     });
-}
+  }
+};
 
-exports.updateTour = (req, res) => {
-    if (req.params.id * 1 > tours.length) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    }
+/**
+ * Creates a new tour and saves it to the database.
+ *
+ * @param {Object} req - The request object containing the tour data.
+ * @param {Object} res - The response object used to send the response.
+ * @return {Promise<void>} - A promise that resolves when the tour is created and the response is sent.
+ */
+exports.createTour = async (req, res) => {
+  try {
+    // const newTour = new Tour({});
+    // newTour.save();
 
+    const newTour = await Tour.create(req.body);
+
+    res.status(201).json({
+      status: 'success',
+      data: {
+        tour: newTour
+      }
+    });
+  } catch (err) {
+    res.status(400).json({
+      status: 'fail',
+      message: err
+    });
+  }
+};
+
+/**
+ * Update a tour by its ID.
+ *
+ * @param {Object} req - the request object
+ * @param {Object} res - the response object
+ * @return {Promise} a Promise that resolves to the updated tour data
+ */
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      new: true,
+      runValidators: true
+    });
     res.status(200).json({
-        status: 'success',
-        data: {
-            tour: 'Updated tour!'
-        }
+      status: 'success',
+      data: {
+        tour
+      }
     });
-}
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
+};
 
-exports.deleteTour = (req, res) => {
-    if (req.params.id * 1 > tours.length) {
-        return res.status(404).json({
-            status: 'fail',
-            message: 'Invalid ID'
-        });
-    }
-
+/**
+ * Deletes a tour by its ID and returns a JSON response with the deleted tour data.
+ *
+ * @param {Object} req - The request object.
+ * @param {Object} res - The response object.
+ * @return {Object} The JSON response with the deleted tour data.
+ */
+exports.deleteTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndDelete(req.params.id);
     res.status(204).json({
-        status: 'success',
-        data: null
+      status: 'success',
+      data: tour
     });
-}
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err
+    });
+  }
+};
