d1967ccdb3b8b8565d236a95c7d586c861b560dc
Pratik Tandel <pratik.tandel@1rivet.comgit>
2024-03-29 12:44:03 +0530

    feat: connect to mongo db using mongoose

--- server.js
+++ server.js
@@ -1,7 +1,51 @@
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+
+dotenv.config({ path: './config.env' });
 const app = require('./app');
 
-/** START SERVER */
-const port = 3000;
+const DB = process.env.DATABASE.replace(
+    '<PASSWORD>',
+    process.env.DATABASE_PASSWORD
+);
+
+mongoose
+    .connect(DB, {
+        useNewUrlParser: true
+    })
+    .then(() => console.log('DB connection successful!'));
+
+const tourSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, 'A tour must have a name'],
+        unique: true
+    },
+    rating: {
+        type: Number,
+        default: 4.5
+    },
+    price: {
+        type: Number,
+        required: [true, 'A tour must have a price']
+    }
+})
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+const testTour = new Tour({
+    name: 'The Forest Hikerr',
+    rating: 4.7,
+    price: 397
+})
+
+testTour.save().then(doc => {
+    console.log(doc);
+}).catch(err => {
+    console.log('ERROR!', err);
+})
+
+const port = process.env.PORT || 3000;
 app.listen(port, () => {
-    console.log(`Server is listening at port ${port}`);
+    console.log(`App running on port ${port}...`);
 });
