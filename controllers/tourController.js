8ccdd6d98d705e2b1f17893d647a2ae0809afd51
Pratik Tandel <pratik.tandel@1rivet.comgit>
2024-04-02 16:49:55 +0530

    feat: implement pagination

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -41,14 +41,26 @@ exports.getAllTours = async (req, res, next) => {
       query = query.select('-__v');
     }
 
+    // 4) Pagination
+    const page = req.query.page * 1 || 1;
+    const limit = req.query.limit * 1 || 100;
+    const skip = (page - 1) * limit;
+
+    query = query.skip(skip).limit(limit);
+
+    if (req.query.page) {
+      const numTours = await Tour.countDocuments();
+      if (skip >= numTours) throw new Error('This page does not exist');
+    }
+
+    /** EXECUTE QUERY */
+    const tours = await query;
+
     // const tours = await Tour.find()
     //   .where('duration')
     //   .equals(5)
     //   .where('difficulty')
     //   .equals('easy');
-
-    /** EXECUTE QUERY */
-    const tours = await query;
 
     /** SEND RESPONSE */
     res.status(200).json({
