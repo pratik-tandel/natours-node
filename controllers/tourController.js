101fdd1e913db3990b60c353212f9f4d5d928a0c
Pratik Tandel <pratik.tandel@1rivet.comgit>
2024-04-02 15:53:04 +0530

    feat: implement sorting

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -10,23 +10,28 @@ const Tour = require('./../models/tourModel');
  */
 exports.getAllTours = async (req, res, next) => {
   try {
-    console.log(req.query);
-
     /** BUILD QUERY */
-    // 1) Filtering
+    // 1A) Filtering
     const queryObj = { ...req.query };
     const excludeFields = ['page', 'sort', 'limit', 'fields'];
     excludeFields.forEach(el => delete queryObj[el]);
 
-    // 2) Advanced Filtering
+    // 1B) Advanced Filtering
     let queryStr = JSON.stringify(queryObj);
     queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, match => `$${match}`);
-    console.log(JSON.parse(queryStr));
 
     // { difficulty: 'easy', duration: { $gte: 5 } }
     // { difficulty: 'easy', duration: { gte: 5 } }
 
-    const query = Tour.find(JSON.parse(queryStr));
+    let query = Tour.find(JSON.parse(queryStr));
+
+    // 2) Sorting
+    if (req.query.sort) {
+      const sortBy = req.query.sort.split(',').join(' ');
+      query = query.sort(sortBy);
+    } else {
+      query = query.sort('-createdAt');
+    }
 
     // const tours = await Tour.find()
     //   .where('duration')
